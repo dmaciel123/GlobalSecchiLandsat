285ac444f0ddadb0156943291a26efa0a34e6d4c
Daniel Maciel <36137468+dmaciel123@users.noreply.github.com>
2023-03-10 10:36:33 -0300

    Update MDN Application.py

--- MDN Application.py
+++ MDN Application.py
@@ -20,6 +20,10 @@ IMAGE_PATH_NC = 'ACOLITE/L9_OLI_2023_02_13_15_41_09_014034_L2W.nc'
 IMAGE_PATH_TIF_REF = 'ACOLITE/L9_OLI_2023_02_13_15_41_09_014034_L2W_Rrs_561.tif'
 SAVE_PATH = 'ACOLITE'
 
-image_MDN_application(IMAGE_PATH_NC, IMAGE_PATH_TIF_REF, SAVE_PATH, 'OLI-NoCoastal', False)
+## Options for sensor: "OLI-NoCoastal": Landsat-8-9/OLI without coastal band
+## Landsat-7/ETM+: ETM, Landsat-5/TM: TM
+
+image_MDN_application(image_NC = IMAGE_PATH_NC, image_ref = IMAGE_PATH_TIF_REF, 
+                      SAVE_FOLDER = SAVE_PATH,  sensor = 'OLI-NoCoastal', useRatio = False)
 
  
