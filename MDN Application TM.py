a5b739cbc1a19740dac86e776fa56d4db8ea4d41
Daniel Maciel <36137468+dmaciel123@users.noreply.github.com>
2023-03-10 10:30:19 -0300

    Update MDN Application TM.py

--- MDN Application TM.py
+++ MDN Application TM.py
@@ -13,7 +13,7 @@ import pandas as pd
 
 #Generate Secchi estimates using MDN for Landsat-5/TM
 
-Data = pd.read_csv(r"C:/Users/damac/Downloads/Final_Model/MDN/Examples/filtered_TM_v2.csv")
+Data = pd.read_csv(r"MDN/Examples/filtered_TM_v2.csv")
 
 print(Data)
 
@@ -49,4 +49,4 @@ final_df = apply_secchi.reset_index()
 final_df['predicted'] = results['Secchi']
 
 print(final_df)
-final_df.to_csv(r"C:/Users/damac/Downloads/Final_Model/MDN/Examples/Zsd_predicted_TM.csv")
+final_df.to_csv(r"MDN/Examples/Zsd_predicted_TM.csv")
